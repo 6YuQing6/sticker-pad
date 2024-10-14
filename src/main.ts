93480675718d1b953cf99c354e58240b5c113306
Sunny Han <shan66@ucsc.edu>
2024-10-13 17:02:14 -0700

    testing again....

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const APP_NAME = "Hello WOrld";
+const APP_NAME = "PLEASE WORK T0T";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
