adfc7ad9f1c444fa9f15e6c1edcb1268d2bc83b2
Sunny Han <shan66@ucsc.edu>
2024-10-13 17:39:50 -0700

    adding drawing function

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,52 @@ const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 app.append(canvas);
+
+let isDrawing = false;
+let x = 0;
+let y = 0;
+const context = canvas.getContext("2d");
+
+// Add the event listeners for mousedown, mousemove, and mouseup
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
+canvas.addEventListener("mousedown", (e) => {
+  x = e.offsetX;
+  y = e.offsetY;
+  isDrawing = true;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+});
+
+document.addEventListener("mouseup", (e) => {
+  if (isDrawing) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+});
+
+function drawLine(
+  context: CanvasRenderingContext2D | null,
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number
+) {
+  if (!context) {
+    return;
+  }
+  context.beginPath();
+  context.strokeStyle = "black";
+  context.lineWidth = 1;
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+  context.closePath();
+}
