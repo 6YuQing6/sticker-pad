78ecd4c2e679a6c06a91d3605cb77c6eb7e27a4c
Sunny Han <shan66@ucsc.edu>
2024-10-15 15:32:05 -0700

    step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -18,12 +18,12 @@ interface Point {
   y: number;
 }
 
-let points: Point[][] = [];
+let lines: Point[][] = [];
 let currentLine: Point[] = [];
 
 let isDrawing = false;
-let x = 0;
-let y = 0;
+let lastX = 0;
+let lastY = 0;
 const context = canvas.getContext("2d");
 
 const clearButton = document.createElement("button");
@@ -31,46 +31,53 @@ clearButton.innerHTML = "clear";
 app.append(clearButton);
 
 clearButton.addEventListener("click", () => {
-  if (!context) {
-    return;
-  }
+  if (!context) return;
   context.clearRect(0, 0, canvas.width, canvas.height);
-  points = [];
+  lines = [];
+  canvas.dispatchEvent(new Event("drawing-changed"));
 });
 
 // Add the event listeners for mousedown, mousemove, and mouseup
-// https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
 canvas.addEventListener("mousedown", (e) => {
-  x = e.offsetX;
-  y = e.offsetY;
+  lastX = e.offsetX;
+  lastY = e.offsetY;
   isDrawing = true;
-  currentLine.push({ x, y });
+  currentLine = [{ x: lastX, y: lastY }];
 });
 
 canvas.addEventListener("mousemove", (e) => {
   if (isDrawing) {
-    drawLine(context, x, y, e.offsetX, e.offsetY);
-    x = e.offsetX;
-    y = e.offsetY;
-    currentLine.push({ x, y });
+    const newX = e.offsetX;
+    const newY = e.offsetY;
+    currentLine.push({ x: newX, y: newY });
+    canvas.dispatchEvent(new Event("drawing-changed"));
   }
 });
 
 canvas.addEventListener("drawing-changed", () => {
-  if (!context) {
-    return;
-  }
+  if (!context) return;
   context.clearRect(0, 0, canvas.width, canvas.height);
-  points.forEach((line) => {
+  lines.forEach((line) => {
     for (let i = 1; i < line.length; i++) {
       drawLine(context, line[i - 1].x, line[i - 1].y, line[i].x, line[i].y);
     }
   });
+  if (currentLine.length > 1) {
+    for (let i = 1; i < currentLine.length; i++) {
+      drawLine(
+        context,
+        currentLine[i - 1].x,
+        currentLine[i - 1].y,
+        currentLine[i].x,
+        currentLine[i].y
+      );
+    }
+  }
 });
 
 document.addEventListener("mouseup", () => {
   if (isDrawing) {
-    points.push(currentLine);
+    lines.push(currentLine);
     currentLine = [];
     isDrawing = false;
     canvas.dispatchEvent(new Event("drawing-changed"));
@@ -84,9 +91,7 @@ function drawLine(
   x2: number,
   y2: number
 ) {
-  if (!context) {
-    return;
-  }
+  if (!context) return;
   context.beginPath();
   context.strokeStyle = "black";
   context.lineWidth = 1;
