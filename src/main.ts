74c6fdb662a893c0923a5531adc30b49c1c49f3b
Sunny Han <shan66@ucsc.edu>
2024-10-26 00:30:13 -0700

    step 11 complete, changing classes to functions, resorting

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,16 @@
 import "./style.css";
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
-document.title = "Sticky Pad";
-
-const title = document.createElement("h1");
-title.innerHTML = "Sticky Pad";
-app.append(title);
 
 const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
-app.append(canvas);
+app.insertBefore(canvas, app.children[1]);
 
-const buttonContainer = document.createElement("div");
-app.append(buttonContainer);
+const buttonContainer =
+  document.querySelector<HTMLDivElement>("#buttonContainer")!;
+
+const toolContainer = document.querySelector<HTMLDivElement>("#toolContainer")!;
 
 interface Displayable {
   display(context: CanvasRenderingContext2D): void;
@@ -25,30 +22,129 @@ interface Point {
   y: number;
 }
 
-let displayList: Displayable[] = [];
-let redoStack: Displayable[] = [];
+const THIN_LINE = 1;
+const THICK_LINE = 4;
+let lineThickness = THIN_LINE;
 let isDrawing = false;
 const lastPoint = { x: 0, y: 0 };
-let lineThickness = 1;
+let displayStack: Displayable[] = [];
+let redoStack: Displayable[] = [];
+let toolPreview: Displayable | null = null;
+let currentDisplayItem: Displayable | null = null;
+let selectedSticker: string | undefined = undefined;
+
 const context = canvas.getContext("2d");
 if (!context) {
   throw new Error("Failed to get 2D context");
 }
-let toolPreview: ToolPreview | null = null;
-let selectedSticker: string | undefined = undefined;
-let currentDisplayItem: Displayable | null = null; // Unified variable
 
-// function buttons
+// Displayable functions
+function createLine(point: Point, thickness: number): Displayable {
+  const points: Point[] = [point];
+
+  function display(context: CanvasRenderingContext2D) {
+    // draws line from previous point to the next point in points array
+    points.reduce((prevPoint, currPoint) => {
+      drawLine(context, prevPoint, currPoint);
+      return currPoint;
+    });
+  }
+
+  function drag(point: Point) {
+    points.push({ x: point.x, y: point.y });
+  }
+
+  function drawLine(
+    context: CanvasRenderingContext2D,
+    start: Point,
+    end: Point
+  ) {
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = thickness;
+    context.moveTo(start.x, start.y);
+    context.lineTo(end.x, end.y);
+    context.stroke();
+    context.closePath();
+  }
+
+  return { display, drag };
+}
+
+function createToolPreview(position: Point, text: string = ""): Displayable {
+  function display(context: CanvasRenderingContext2D) {
+    if (text) {
+      // for sticker preview
+      context.font = "32px monospace";
+      context.fillText(text, position.x, position.y);
+    } else {
+      // for line width preview, draws a circle
+      context.beginPath();
+      context.arc(position.x, position.y, lineThickness, 0, Math.PI * 2);
+      context.strokeStyle = "red";
+      context.lineWidth = 3;
+      context.stroke();
+      context.closePath();
+    }
+  }
+
+  function drag() {}
+
+  return { display, drag };
+}
+
+function createSticker(position: Point, text: string): Displayable {
+  function display(context: CanvasRenderingContext2D) {
+    context.font = "32px monospace";
+    context.fillText(text, position.x, position.y);
+  }
+
+  function drag(point: Point) {
+    position.x = point.x;
+    position.y = point.y;
+  }
+
+  return { display, drag };
+}
+
+// Utility functions
+function createButton(
+  label: string,
+  container: HTMLElement,
+  onClick: () => void
+) {
+  const button = document.createElement("button");
+  button.innerHTML = label;
+  button.addEventListener("click", onClick);
+  container.append(button);
+  return button;
+}
+
+const createStickerButton = (text: string, container: HTMLElement) => {
+  const stickerButton = createButton(text, container, () => {
+    toggleButtonSelection(stickerButton);
+    selectedSticker = text;
+  });
+};
+
+function toggleButtonSelection(selectedButton: HTMLButtonElement) {
+  const buttons = toolContainer.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.classList.toggle("selected", button === selectedButton);
+  });
+}
+
+// Button creation
 createButton("clear", buttonContainer, () => {
   context.clearRect(0, 0, canvas.width, canvas.height);
-  displayList = [];
+  displayStack = [];
   redoStack = [];
   canvas.dispatchEvent(new Event("drawing-changed"));
   currentDisplayItem = null;
 });
 
 createButton("undo", buttonContainer, () => {
-  const line = displayList.pop();
+  const line = displayStack.pop();
   if (line) {
     redoStack.push(line);
     canvas.dispatchEvent(new Event("drawing-changed"));
@@ -58,7 +154,7 @@ createButton("undo", buttonContainer, () => {
 createButton("redo", buttonContainer, () => {
   const line = redoStack.pop();
   if (line) {
-    displayList.push(line);
+    displayStack.push(line);
     canvas.dispatchEvent(new Event("drawing-changed"));
   }
 });
@@ -76,7 +172,7 @@ createButton("export", buttonContainer, () => {
   exportContext.scale(4, 4);
 
   // Execute display list items on the new context
-  displayList.forEach((displayable) => {
+  displayStack.forEach((displayable) => {
     displayable.display(exportContext);
   });
 
@@ -87,31 +183,21 @@ createButton("export", buttonContainer, () => {
   anchor.click();
 });
 
-// tool buttons
-const toolContainer = document.createElement("div");
-app.append(toolContainer);
-
 const thinButton = createButton("thin", toolContainer, () => {
-  lineThickness = 1;
+  lineThickness = THIN_LINE;
   toggleButtonSelection(thinButton);
   currentDisplayItem = null;
   selectedSticker = "";
 });
 thinButton.className = "selected";
 const thickButton = createButton("thick", toolContainer, () => {
-  lineThickness = 5;
+  lineThickness = THICK_LINE;
   toggleButtonSelection(thickButton);
   currentDisplayItem = null;
   selectedSticker = "";
 });
 
-const createStickerButton = (text: string, container: HTMLElement) => {
-  const stickerButton = createButton(text, container, () => {
-    toggleButtonSelection(stickerButton);
-    selectedSticker = text;
-  });
-};
-
+// create new sticker button
 createButton("+", toolContainer, () => {
   const text = globalThis.prompt("Custom sticker prompt", "❤️");
   if (text) {
@@ -119,113 +205,9 @@ createButton("+", toolContainer, () => {
   }
 });
 
-const stickerDisplay: string[] = ["😆", "🍔", "✨"];
-stickerDisplay.forEach((sticker) => {
+["😆", "🍔", "✨"].forEach((sticker) => {
   createStickerButton(sticker, toolContainer);
 });
-
-function createButton(
-  label: string,
-  container: HTMLElement,
-  onClick: () => void
-) {
-  const button = document.createElement("button");
-  button.innerHTML = label;
-  button.addEventListener("click", onClick);
-  container.append(button);
-  return button;
-}
-
-function toggleButtonSelection(selectedButton: HTMLButtonElement) {
-  const buttons = toolContainer.querySelectorAll("button");
-  buttons.forEach((button) => {
-    button.classList.toggle("selected", button === selectedButton);
-  });
-}
-
-class Line implements Displayable {
-  points: Point[];
-  thickness: number;
-  constructor(point: Point, thickness: number) {
-    this.points = [point];
-    this.thickness = thickness;
-  }
-
-  display(context: CanvasRenderingContext2D) {
-    this.points.reduce((prevPoint, currPoint) => {
-      this.drawLine(context, prevPoint, currPoint);
-      return currPoint;
-    });
-  }
-
-  drag(point: Point) {
-    this.points.push({ x: point.x, y: point.y });
-  }
-
-  private drawLine(
-    context: CanvasRenderingContext2D,
-    start: Point,
-    end: Point
-  ) {
-    context.beginPath();
-    context.strokeStyle = "black";
-    context.lineWidth = this.thickness;
-    context.moveTo(start.x, start.y);
-    context.lineTo(end.x, end.y);
-    context.stroke();
-    context.closePath();
-  }
-}
-
-class ToolPreview implements Displayable {
-  position: Point;
-  text: string;
-
-  constructor(position: Point, text: string = "") {
-    this.position = position;
-    this.text = text;
-  }
-
-  display(context: CanvasRenderingContext2D) {
-    if (this.text) {
-      // for sticker preview
-      context.font = "32px monospace";
-      context.fillText(this.text, this.position.x, this.position.y);
-    } else {
-      // for line width preiew, draws a circle
-      context.beginPath();
-      context.arc(
-        this.position.x,
-        this.position.y,
-        lineThickness,
-        0,
-        Math.PI * 2
-      );
-      context.strokeStyle = "red";
-      context.lineWidth = 3;
-      context.stroke();
-      context.closePath();
-    }
-  }
-  drag(): void {}
-}
-
-class Sticker implements Displayable {
-  position: Point;
-  text: string;
-  constructor(position: Point, text: string) {
-    this.position = position;
-    this.text = text;
-  }
-  display(context: CanvasRenderingContext2D): void {
-    context.font = "32px monospace";
-    context.fillText(this.text, this.position.x, this.position.y);
-  }
-  drag(point: Point) {
-    this.position.x = point.x;
-    this.position.y = point.y;
-  }
-}
 
 canvas.addEventListener("mouseleave", () => {
   toolPreview = null;
@@ -238,12 +220,12 @@ canvas.addEventListener("mousedown", (e: MouseEvent) => {
   isDrawing = true;
   redoStack = [];
   if (selectedSticker) {
-    currentDisplayItem = new Sticker(
+    currentDisplayItem = createSticker(
       { x: lastPoint.x, y: lastPoint.y },
       selectedSticker
     );
   } else {
-    currentDisplayItem = new Line(
+    currentDisplayItem = createLine(
       { x: lastPoint.x, y: lastPoint.y },
       lineThickness
     );
@@ -254,7 +236,7 @@ canvas.addEventListener("mousedown", (e: MouseEvent) => {
 canvas.addEventListener("mousemove", (e: MouseEvent) => {
   if (!isDrawing) {
     // creates new tool preview of selected tool
-    toolPreview = new ToolPreview(
+    toolPreview = createToolPreview(
       { x: e.offsetX, y: e.offsetY },
       selectedSticker
     );
@@ -270,7 +252,7 @@ canvas.addEventListener("mousemove", (e: MouseEvent) => {
 
 document.addEventListener("mouseup", () => {
   if (!isDrawing) return;
-  if (currentDisplayItem) displayList.push(currentDisplayItem);
+  if (currentDisplayItem) displayStack.push(currentDisplayItem);
   currentDisplayItem = null;
   isDrawing = false;
   canvas.dispatchEvent(new Event("drawing-changed"));
@@ -278,7 +260,7 @@ document.addEventListener("mouseup", () => {
 
 const redrawCanvas = () => {
   context?.clearRect(0, 0, canvas.width, canvas.height);
-  displayList.forEach((displayable) => {
+  displayStack.forEach((displayable) => {
     displayable.display(context);
   });
   currentDisplayItem?.display(context);
